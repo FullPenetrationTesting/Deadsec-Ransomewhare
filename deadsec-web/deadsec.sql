6b6876ed8c478044714fc91af146078bcd1fc4a5
Vicky <70372660+FullPenetrationTesting@users.noreply.github.com>
2023-09-16 20:24:17 +0530

    Add files via upload

---
+++ deadsec-web/deadsec.sql
@@ -0,0 +1,9 @@
+
+CREATE TABLE `deadsec_keys` (
+  `software_key` char(32) NOT NULL,
+  `decrypt_key` char(32) NOT NULL,
+  `date` datetime NOT NULL,
+  `payment` boolean NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+
